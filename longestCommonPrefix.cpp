48cb3d0bf7965f84ece1a72d28c8b0b1b34eb999
Parth <parth.manocha2901@gmail.com>
2026-05-10 11:48:17 +0530

    Implemented LongestCommonPrefix- LeetCode Functionality

---
+++ longestCommonPrefix.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string longestCommonPrefix(vector<string> &strs)
+
+{
+    if (strs.empty())
+        return "";
+
+    string prefix = strs[0];
+
+    for (int i = 1; i < strs.size(); i++)
+    {
+        while (strs[i].find(prefix) != 0)
+        {
+            prefix.pop_back();
+
+            if (prefix.empty())
+                return "";
+        }
+    }
+
+    return prefix;
+};
+int main()
+{
+    vector<string> strs={"app","apple"};
+    cout<<longestCommonPrefix(strs);
+    return 0;
+}
